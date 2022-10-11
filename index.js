bc5e1d0908b3230f39bb9449e2c910442c85a60f
jbilander <jorgen.bilander@gmail.com>
2022-10-11 14:43:49 +0200

    fixed styling of Navbar

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Main js file
+$(document).ready(function () {
+
+    $(".navbar .nav-link").on("click", function () {
+        $(".navbar").find(".active").removeClass("active");
+        $(this).addClass("active");
+    });
+
+});
