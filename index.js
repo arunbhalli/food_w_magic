6b75a7acd77b6d9b2f50cb74744e66820edaf8f5
arunbhalli <tomarbhalli@gmail.com>
2022-10-12 07:32:11 +0200

    update main index.js file

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Main js file
+// Main js file// Main js file
+function showMap()
+{
+    $("#map").toggleClass("d-none", "d-block");
+}
+
+$(document).ready(function () {
+
+    $(".navbar .nav-link").on("click", function () {
+        $(".navbar").find(".active").removeClass("active");
+        $(this).addClass("active");
+    });
+});
+
