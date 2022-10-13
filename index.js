249918909d5efee5eb6e9b498cce71ff4378f0b6
arunbhalli <tomarbhalli@gmail.com>
2022-10-13 20:04:20 +0200

    fixed menu according to tablet size and laptop size

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 // Main js file
 function showMap() {
-  $("#map").toggleClass("d-none", "d-block");
+  $('#map').toggleClass('d-none', 'd-block');
 }
 
 $(document).ready(function () {
-  $(".navbar .nav-link").on("click", function () {
-    $(".navbar").find(".active").removeClass("active");
-    $(this).addClass("active");
+  $('.navbar .nav-link').on('click', function () {
+    $('.navbar').find('.active').removeClass('active');
+    $(this).addClass('active');
   });
-  $(".add .carousel").carousel({
+  $('.add .carousel').carousel({
     interval: 1200,
   });
 });
 
-
 $(document).ready(function () {
-
   /************Calculate Menu START****************/
-  $("#menu .addsign ").click(function () {
+  $('#menu .addsign ').click(function () {
     $(this).parent().addClass('active-item');
 
     //add 1 to quantity
@@ -26,10 +24,9 @@ $(document).ready(function () {
     calculateTotal();
   });
 
-  $("#menu .subtract ").click(function () {
-
+  $('#menu .subtract ').click(function () {
     let qty = Number($(this).parent().find('.qty').text());
-    qty = qty > 0 ? qty - 1 : 0;  //Subtract only if qty is larger than zero, otherwise set zero
+    qty = qty > 0 ? qty - 1 : 0; //Subtract only if qty is larger than zero, otherwise set zero
 
     if (qty < 1) {
       $(this).parent().removeClass('active-item');
@@ -40,19 +37,16 @@ $(document).ready(function () {
   });
 
   function calculateTotal() {
-
     let sum = 0;
 
-    $("#menu .active-item").each(function () {
-
+    $('#menu .active-item').each(function () {
       let price = Number($(this).find('.price').text());
       let qty = Number($(this).find('.qty').text());
 
       sum = sum + price * qty;
     });
 
-    $("#amount").text(sum);
+    $('#amount').text('Total Amount: ' + sum + ' kr');
   }
   /************Calculate Menu END*****************/
-
 });
